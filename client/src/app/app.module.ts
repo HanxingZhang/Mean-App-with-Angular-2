2ecb132db650050d24b8f4ba090b4be6e97db966
Hanxing Zhang <zhanghanxing026@gmail.com>
2018-02-25 11:48:41 -0800

    register form

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';  // <-- #1 import module
 import { AppComponent } from './app.component';
 import {AppRoutingModule} from "./app-routing.module";
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { RegisterComponent } from './components/register/register.component';
 // import {FormsModule} from "@angular/forms";
 // import {HttpModule} from "@angular/http";
 
@@ -16,10 +17,12 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
     NavbarComponent,
     HomeComponent,
     DashboardComponent,
+    RegisterComponent,
 
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     // FormsModule,
     // HttpModule,
     AppRoutingModule
