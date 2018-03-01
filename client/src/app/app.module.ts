4be779298bd0f167d6bc4cf92c053481d2e787d2
Hanxing Zhang <zhanghanxing026@gmail.com>
2018-03-01 13:50:46 -0800

    register2

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';  // <-- #1 import module
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {AppRoutingModule} from "./app-routing.module";
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { RegisterComponent } from './components/register/register.component';
-// import {FormsModule} from "@angular/forms";
-// import {HttpModule} from "@angular/http";
-
+import { AuthService } from './services/auth.service';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -18,17 +18,15 @@ import { RegisterComponent } from './components/register/register.component';
     HomeComponent,
     DashboardComponent,
     RegisterComponent,
-
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     ReactiveFormsModule,
-    // FormsModule,
-    // HttpModule,
     AppRoutingModule
-
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- routes/authentication.js
+++ routes/authentication.js
@@ -62,41 +62,90 @@ module.exports = (router) => {
         }
     });
 
+    /* ============================================================
+       Route to check if user's email is available for registration
+    ============================================================ */
+    router.get('/checkEmail/:email', (req, res) => {
+        // Check if email was provided in paramaters
+        if (!req.params.email) {
+            res.json({ success: false, message: 'E-mail was not provided' }); // Return error
+        } else {
+            // Search for user's e-mail in database;
+            User.findOne({ email: req.params.email }, (err, user) => {
+                if (err) {
+                    res.json({ success: false, message: err }); // Return connection error
+                } else {
+                    // Check if user's e-mail is taken
+                    if (user) {
+                        res.json({ success: false, message: 'E-mail is already taken' }); // Return as taken e-mail
+                    } else {
+                        res.json({ success: true, message: 'E-mail is available' }); // Return as available e-mail
+                    }
+                }
+            });
+        }
+    });
+
+    /* ===============================================================
+       Route to check if user's username is available for registration
+    =============================================================== */
+    router.get('/checkUsername/:username', (req, res) => {
+        // Check if username was provided in paramaters
+        if (!req.params.username) {
+            res.json({ success: false, message: 'Username was not provided' }); // Return error
+        } else {
+            // Look for username in database
+            User.findOne({ username: req.params.username }, (err, user) => {
+                // Check if connection error was found
+                if (err) {
+                    res.json({ success: false, message: err }); // Return connection error
+                } else {
+                    // Check if user's username was found
+                    if (user) {
+                        res.json({ success: false, message: 'Username is already taken' }); // Return as taken username
+                    } else {
+                        res.json({ success: true, message: 'Username is available' }); // Return as vailable username
+                    }
+                }
+            });
+        }
+    });
+
+
+    router.post('/login', (req, res) => {
+        // Check if username was provided
+        if (!req.body.username) {
+            res.json({ success: false, message: 'No username was provided' }); // Return error
+        } else {
+            // Check if password was provided
+            if (!req.body.password) {
+                res.json({ success: false, message: 'No password was provided.' }); // Return error
+            } else {
+                // Check if username exists in database
+                User.findOne({ username: req.body.username.toLowerCase() }, (err, user) => {
+                    // Check if error was found
+                    if (err) {
+                        res.json({ success: false, message: err }); // Return error
+                    } else {
+                        // Check if username was found
+                        if (!user) {
+                            res.json({ success: false, message: 'Username not found.' }); // Return error
+                        } else {
+                            const validPassword = user.comparePassword(req.body.password); // Compare password provided to password in database
+                            // Check if password is a match
+                            if (!validPassword) {
+                                res.json({ success: false, message: 'Password invalid' }); // Return error
+                            } else {
+                                // const token = jwt.sign({ userId: user._id }, config.secret, { expiresIn: '24h' }); // Create a token for client
+                                // res.json({ success: true, message: 'Success!', token: token, user: { username: user.username } }); // Return success and token to frontend
+                                res.json({ success: true, message: 'Success!'});
+                            }
+                        }
+                    }
+                });
+            }
+        }
+    });
+
     return router; // Return router object to main index.js
 }
-
-// const User = require('../models/user'); // Import User Model Schema
-//
-// module.exports = (router)=> {
-//     router.post('/register', (req, res) =>{
-//
-//         if(!req.body.email){
-//             res.json({ success: false, message: 'You must provide an e-mail'});
-//         }else {
-//             if(!req.body.username){
-//                 res.json({ success:false, message: 'You must provide a username' });
-//             }else{
-//                 if(!req.body.password){
-//                     res.json({ success: false, message: 'You must provide a password'});
-//                 }else {
-//                     let user = new User({
-//                         email: req.body.email.toLowerCase(),
-//                         username: req.body.username.toLowerCase(),
-//                         password: req.body.password
-//                     });
-//                     user.save((err) => {
-//                         if (err) {
-//                             console.log(err);
-//                             res.json({ success: false, message: 'Could not save user.Error:', err});
-//                         }else{
-//                             res.json({success: true, message: 'User saved!'});
-//                         }
-//                     })
-//                 }
-//
-//             }
-//         }
-//
-//     });
-//     return router;
-// }
